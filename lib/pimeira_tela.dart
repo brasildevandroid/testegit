7f9a409719ee2229c494c2c54e5e848b85d62985
Luiz Pinheiro <luozcox1@gmail.com>
2022-12-26 20:41:08 -0300

    add_primeira_tela

---
+++ lib/pimeira_tela.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SegundaTela extends StatelessWidget {
+  const SegundaTela({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
