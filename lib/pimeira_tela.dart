3f3ace6192d0e5c0643a76a52721a8e81e3209e9
Luiz Pinheiro <luozcox1@gmail.com>
2023-01-18 19:54:05 -0300

    nova_atualizacao

--- lib/pimeira_tela.dart
+++ lib/pimeira_tela.dart
@@ -5,6 +5,6 @@ class SegundaTela extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text('Adicionando titulo'),);
+    return Container(child: Text('Adicionando titulosfddsfdsf'),);
   }
 }
